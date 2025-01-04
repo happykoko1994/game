aa653d6496bcfe3ea85ae722a218e3d91e51f1a1
Maksim <max-goryachev@ya.ru>
2025-01-04 13:52:48 +0500

    фыв

--- backend/questions.js
+++ backend/questions.js
@@ -1,26 +1,42 @@
 module.exports = [
     {
-        text: "What is the capital of France?",
+        text: "Какая столица Франции?",
         answers: [
-            { text: "Paris", points: 50 },
-            { text: "Lyon", points: 30 },
-            { text: "Marseille", points: 20 },
+            { text: "Париж", points: 50 },
+            { text: "Лион", points: 30 },
+            { text: "Марсель", points: 20 },
         ],
     },
     {
-        text: "What is the largest planet in our solar system?",
+        text: "Какая самая большая планета в нашей Солнечной системе?",
         answers: [
-            { text: "Jupiter", points: 50 },
-            { text: "Saturn", points: 30 },
-            { text: "Neptune", points: 20 },
+            { text: "Юпитер", points: 50 },
+            { text: "Сатурн", points: 30 },
+            { text: "Нептун", points: 20 },
         ],
     },
     {
-        text: "Name a common pet animal?",
+        text: "Назови популярного домашнего питомца.",
         answers: [
-            { text: "Dog", points: 50 },
-            { text: "Cat", points: 40 },
-            { text: "Fish", points: 30 },
+            { text: "Собака", points: 50 },
+            { text: "Кошка", points: 40 },
+            { text: "Рыба", points: 30 },
+        ],
+    },
+    {
+        text: "Какой элемент имеет символ 'O' в периодической таблице?",
+        answers: [
+            { text: "Кислород", points: 50 },
+            { text: "Озер", points: 30 },
+            { text: "Огнезащитный", points: 20 },
+        ],
+    },
+    {
+        text: "Как называется самая высокая гора в мире?",
+        answers: [
+            { text: "Эверест", points: 50 },
+            { text: "Канченджанга", points: 30 },
+            { text: "Дхаулагири", points: 20 },
         ],
     },
 ];
