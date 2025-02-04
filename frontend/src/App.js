48221411149a275ca9e3046e2069d75239c09495
Maksim <max-goryachev@ya.ru>
2025-02-04 14:29:06 +0500

    qwe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { socket } from "./socket";
+import "./App.css";
 import GameRoom from "./components/GameRoom";
 import WelcomeScreen from "./components/WelcomeScreen";
 import DayNightCycle from "./components/DayNightCycle";
 
 export default function App() {
   const [name, setName] = useState(localStorage.getItem("playerName"));
-  const [score] = useState(JSON.parse(localStorage.getItem("playerScore")) || 0);
-  const [answered] = useState(JSON.parse(localStorage.getItem("playerAnswered")) || false);
+  const [score] = useState(
+    JSON.parse(localStorage.getItem("playerScore")) || 0
+  );
+  const [answered] = useState(
+    JSON.parse(localStorage.getItem("playerAnswered")) || false
+  );
 
   useEffect(() => {
     if (name) {
@@ -16,7 +21,7 @@ export default function App() {
   }, [name]);
 
   return (
-    <DayNightCycle>
+    <div>
       {name ? (
         <GameRoom />
       ) : (
@@ -27,7 +32,18 @@ export default function App() {
           }}
         />
       )}
-    </DayNightCycle>
+      <div
+        className="tenor-gif-embed"
+        data-postid="24485314"
+        data-share-method="host"
+        data-aspect-ratio="1.77778"
+        data-width="100%"
+      ></div>{" "}
+      <script
+        type="text/javascript"
+        async
+        src="https://tenor.com/embed.js"
+      ></script>
+    </div>
   );
 }
-
