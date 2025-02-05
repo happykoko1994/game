0b3e6738fca049d35a609d10e7f18e3260f8311c
Maksim <max-goryachev@ya.ru>
2025-02-05 20:00:44 +0500

    йцу

--- frontend/src/components/Logs.js
+++ frontend/src/components/Logs.js
@@ -1,26 +1,26 @@
-import React, { useEffect, useRef } from 'react';
-import styles from '../style/Logs.module.css';
+import React, { useEffect, useRef } from "react";
+import styles from "../style/Logs.module.css";
 
 export default function Logs({ logs }) {
-    const logsEndRef = useRef(null);
+  const logsEndRef = useRef(null);
 
-    useEffect(() => {
-        // Прокручиваем вниз, как только обновляются логи
-        if (logsEndRef.current) {
-            logsEndRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    }, [logs]); // Зависимость от изменения логов
+  useEffect(() => {
+    // Прокручиваем вниз, как только обновляются логи
+    if (logsEndRef.current) {
+      logsEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [logs]); // Зависимость от изменения логов
 
-    return (
-        <div className={styles.logsContainer}>
-            <ul className={styles.logsList}>
-                {logs.map((log, index) => (
-                    <li key={index} className={styles.logItem}>
-                        <span dangerouslySetInnerHTML={{ __html: log }} />
-                    </li>
-                ))}
-            </ul>
-            <div ref={logsEndRef} /> {/* Сюда будет прокручиваться */}
-        </div>
-    );
+  return (
+    <div className={styles.logsContainer}>
+      <ul className={styles.logsList}>
+        {logs.map((log, index) => (
+          <li key={index} className={styles.logItem}>
+            <span dangerouslySetInnerHTML={{ __html: log }} />
+          </li>
+        ))}
+        <div ref={logsEndRef} /> {/* Сюда будет прокручиваться */}
+      </ul>
+    </div>
+  );
 }
